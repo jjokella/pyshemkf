e6c6c666812de6b8eb6509187d5cd123deca2bba
jk125262 <jk125262@cluster2.rz.RWTH-Aachen.DE>
2015-05-22 16:35:07 +0200

    Adding module part to make small movies.

---
+++ mypackage/plot/movies.py
@@ -0,0 +1,36 @@
+import shlex
+import exceptions
+import os
+from mypackage.run import runmodule as rm
+from mypackage.plot import myplots
+import time
+python_dir = '/home/jk125262/PythonDir_Cluster'
+
+def movie(input_name_test = 'plot_m_b_01_100_av.png', input_names = 'plot_m_b_%02d_100_av.png',
+          output_name = 'plot_m_b_100_av.mp4', model_name = 'wave', date = '2015_05_19'):
+
+    # Directory
+    pics_dir = '/home/jk125262/shematOutputDir_Cluster/' + model_name \
+        + '_output/' + date + '/pics/'
+    
+    # Input test
+    os.chdir(pics_dir)
+    if not os.path.isfile(input_name_test):
+        os.chdir(python_dir)
+        raise exceptions.RuntimeError, 'File' + input_name_test +  ' not found in ' + pics_dir + '!!!'
+    # Output deletion if exists
+    os.chdir(pics_dir)
+    if os.path.isfile(output_name):
+        os.remove(output_name)
+    
+    
+    # Shell command split by the shlex utility
+    arg = shlex.split('ffmpeg -qscale 5 -r 2 -b 9600 -i ' + input_names + ' ' + output_name)
+
+    # The shell command run
+    rm.run_script(pics_dir,arg,wait = 1, errout = 1)
+
+    os.chdir(python_dir)
+
+print('\n Done with module : movies.py.')
+print(time.asctime( time.localtime( time.time())))
