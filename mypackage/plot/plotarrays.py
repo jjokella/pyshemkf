41d3313b944b16030356da3257e4fd55af61e43e
jk125262 <jkeller@eonerc.rwth-aachen.de>
2015-12-15 14:06:37 +0100

    mypackage.plot: plotarrays.py containing universal arrays for scripts
    Arrays like: dats, lets, nums, num_obss and the names of the different
    methods are collected in this module and used by various of the plot
    scripts.

---
+++ mypackage/plot/plotarrays.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+
+# Paths
+python_dir = '/home/jk125262/PythonDir_Cluster'
+
+import numpy as np
+
+num_methods = 17
+num_jobs = num_methods*6
+
+num_methods1000 = 4
+num_jobs1000 = num_methods1000*3
+
+# Date of output directory
+normal_dats = ['2015_06_10','2015_06_11','2015_06_11','2015_06_16','2015_06_16','2015_06_17']
+normalscore_dats = ['2015_06_11','2015_06_12','2015_06_12','2015_06_19','2015_06_19','2015_06_19']
+damping_dats = ['2015_06_13','2015_06_14','2015_06_14','2015_06_22','2015_06_22','2015_06_22']
+localisation_dats = ['2015_06_23','2015_06_23','2015_06_23','2015_06_24','2015_06_24','2015_06_24']
+dual_dats = ['2015_07_15','2015_07_15','2015_07_15','2015_07_15','2015_07_15','2015_07_15']
+damping03_dats = ['2015_07_16','2015_07_16','2015_07_16','2015_07_17','2015_07_17','2015_07_17']
+damping01_dats = ['2015_07_16','2015_07_16','2015_07_16','2015_07_17','2015_07_17','2015_07_17']
+normalscorenew_dats = ['2015_09_04','2015_09_04','2015_09_04','2015_09_04','2015_09_04','2015_09_04']
+normalscoreverynew_dats = ['2015_10_01','2015_10_01','2015_10_01','2015_10_01','2015_10_01','2015_10_01']
+hybrid_dats = ['2015_10_02','2015_10_02','2015_10_02','2015_10_02','2015_10_02','2015_10_02']
+damping02_dats = ['2015_10_05','2015_10_05','2015_10_05','2015_10_05','2015_10_05','2015_10_05']
+iterative_dats = ['2015_10_16','2015_10_16','2015_10_16','2015_10_16','2015_10_16','2015_10_16']
+iterative_2_dats = ['2015_10_16','2015_10_16','2015_10_16','2015_10_16','2015_10_16','2015_10_16']
+newiterative_dats = ['2015_11_18','2015_11_18','2015_11_18','2015_11_18','2015_11_18','2015_11_18']
+newiterative2_dats = ['2015_11_23','2015_11_23','2015_11_23','2015_11_23','2015_11_23','2015_11_23']
+newiterative3_dats = ['2015_12_08','2015_12_08','2015_12_08','2015_12_09','2015_12_09','2015_12_09']
+newiterative4_dats = ['2015_12_11','2015_12_11','2015_12_11','2015_12_11','2015_12_11','2015_12_11']
+obs_10_normal_dats = ['2015_07_20','2015_07_20','2015_07_20','2015_07_20','2015_07_20','2015_07_20']
+obs_200_normal_dats = ['2015_07_22','2015_07_22','2015_07_22','2015_07_22','2015_07_22','2015_07_22']
+lowobsvar_dats = ['2015_07_21','2015_07_27','2015_07_27','2015_07_26','2015_07_26','2015_07_26']
+
+normal1000_dats = ['2015_11_26','2015_11_26','2015_11_26']
+damping1000_dats = ['2015_11_27','2015_11_27','2015_11_27']
+normalscore1000_dats = ['2015_12_05','2015_12_05','2015_12_05']
+dual1000_dats = ['2015_12_05','2015_12_05','2015_12_05']
+
+# First letter of runs
+normal_lets = ['b','b','cx','cx','gt','b']
+normalscore_lets = ['gt','b','cx','b','cx','gt']
+damping_lets = ['b','b','cx','b','cx','gt']
+localisation_lets = ['b','cx','gt','b','cx','gt']
+dual_lets = ['b','cx','gt','kp','ol','sh']
+damping03_lets = ['b','cx','gt','b','cx','gt']
+damping01_lets = ['kp','ol','sh','kp','ol','sh']
+normalscorenew_lets = ['b','cx','gt','kp','ol','sh']
+normalscoreverynew_lets = ['b','cx','gt','kp','ol','sh']
+hybrid_lets = ['b','cx','gt','kp','ol','sh']
+damping02_lets = ['b','cx','gt','kp','ol','sh']
+iterative_lets = ['b','cx','gt','kp','ol','sh']
+iterative_2_lets = ['b','cx','gt','kp','ol','sh']
+newiterative_lets = ['b','cx','gt','kp','ol','sh']
+newiterative2_lets = ['b','cx','gt','kp','ol','sh']
+newiterative3_lets = ['b','aln','bxz','dkl','ewx','gjj']
+newiterative4_lets = ['b','aln','bxz','dkl','ewx','gjj']
+obs_10_normal_lets = ['b','cx','gt','kp','ol','sh']
+obs_200_normal_lets = ['b','cx','gt','kp','ol','sh']
+lowobsvar_lets = ['b','cx','gt','kp','ol','sh']
+
+normal1000_lets = ['b','aln','bxz']
+damping1000_lets = ['b','aln','bxz']
+normalscore1000_lets = ['b','aln','bxz']
+dual1000_lets = ['dkl','ewx','gjj']
+
+# Number of runs
+normal_nums = [100,100,100,100,100,100]
+normalscore_nums = [100,100,100,70,48,28]  
+damping_nums = [100,100,100,100,100,100]
+localisation_nums = [100,100,100,100,100,100]
+dual_nums = [100,100,100,100,100,100]
+damping03_nums = [100,100,100,100,100,100]
+damping01_nums = [100,100,100,100,100,100]
+normalscorenew_nums = [100,100,100,40,97,34]
+normalscoreverynew_nums = [100,100,100,100,100,34] 
+hybrid_nums = [100,100,100,100,100,100]
+damping02_nums = [100,100,100,100,100,100]
+iterative_nums = [100,100,100,100,100,100]
+iterative_2_nums = [100,100,100,100,100,100]
+newiterative_nums = [100,100,100,100,100,100]
+newiterative2_nums = [100,100,100,100,100,100]
+newiterative3_nums = [99,100,100,100,100,64]
+newiterative4_nums = [99,88,22,16,8,4]
+obs_10_normal_nums = [100,100,100,100,100,100]
+obs_200_normal_nums = [100,100,100,100,100,100]
+lowobsvar_nums = [100,100,100,100,100,100]
+
+normal1000_nums = [1000,1000,1000]
+damping1000_nums = [1000,1000,1000]
+normalscore1000_nums = [1000,1000,641]
+dual1000_nums = [1000,665,872]
+
+dats = [normal_dats, normalscore_dats, damping_dats, localisation_dats, dual_dats, damping03_dats, 
+        damping01_dats, normalscorenew_dats, normalscoreverynew_dats, hybrid_dats, damping02_dats,
+        iterative_dats, iterative_2_dats, newiterative_dats,newiterative2_dats,
+        newiterative3_dats, newiterative4_dats,
+        obs_10_normal_dats, obs_200_normal_dats, lowobsvar_dats]
+lets = [normal_lets, normalscore_lets, damping_lets, localisation_lets, dual_lets, damping03_lets,
+        damping01_lets, normalscorenew_lets, normalscoreverynew_lets, hybrid_lets, damping02_lets,
+        iterative_lets, iterative_2_lets, newiterative_lets,newiterative2_lets,
+        newiterative3_lets,newiterative4_lets,
+        obs_10_normal_lets, obs_200_normal_lets, lowobsvar_lets]
+nums = [normal_nums, normalscore_nums, damping_nums, localisation_nums, dual_nums, damping03_nums,
+        damping01_nums, normalscorenew_nums, normalscoreverynew_nums, hybrid_nums, damping02_nums,
+        iterative_nums, iterative_2_nums, newiterative_nums,newiterative2_nums,
+        newiterative3_nums,newiterative4_nums,
+        obs_10_normal_nums, obs_200_normal_nums, lowobsvar_nums]
+
+# Number of observation intervals
+num_obss = [[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],
+            [100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],
+            [100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],
+            [100 for i in range(6)],[150 for i in range(6)],[ 55 for i in range(6)],[ 55 for i in range(5)],
+            [5050 for i in range(6)],
+            [ 10 for i in range(6)],[200 for i in range(6)],
+            [100 for i in range(6)]]                             #Should be almost constant
+
+
+dats1000 = [normal1000_dats,damping1000_dats,normalscore1000_dats,dual1000_dats]
+lets1000 = [normal1000_lets,damping1000_lets,normalscore1000_lets,dual1000_lets]
+nums1000 = [normal1000_nums,damping1000_nums,normalscore1000_nums,dual1000_nums]
+
+# Flat arrays
+dates = np.concatenate(dats)           #Dates
+letters = np.concatenate(lets)         #Letters
+sizes = np.concatenate(nums)           #Number of runs
+
+dates1000 = np.concatenate(dats1000)
+letters1000 = np.concatenate(lets1000)
+sizes1000 = np.concatenate(nums1000)
+
+# names = ['EnKF 50', 'EnKF 100', 'EnKF 250', 'EnKF 500', 'EnKF 1000', 'EnKF 2000',
+#          'NS-EnKF 50', 'NS-EnKF 100', 'NS-EnKF 250', 'NS-EnKF 500', 'NS-EnKF 1000', 'NS-EnKF 2000',
+#          'Dam 0.5 50', 'Dam 0.5 100', 'Dam 0.5 250', 'Dam 0.5 500', 'Dam 0.5 1000', 'Dam 0.5 2000',
+#          'LEnKF 50', 'LEnKF 100', 'LEnKF 250', 'LEnKF 500', 'LEnKF 1000', 'LEnKF 2000',
+#          'Dual EnKF 50', 'Dual EnKF 100', 'Dual EnKF 250', 'Dual EnKF 500', 'Dual EnKF 1000', 'Dual EnKF 2000',
+#          'Dam 0.3 50', 'Dam 0.3 100', 'Dam 0.3 250', 'Dam 0.3 500', 'Dam 0.3 1000', 'Dam 0.3 2000',
+#          'Dam 0.1 50', 'Dam 0.1 100', 'Dam 0.1 250', 'Dam 0.1 500', 'Dam 0.1 1000', 'Dam 0.1 2000',
+#          'NS-EnKF2 50', 'NS-EnKF2 100', 'NS-EnKF2 250', 'NS-EnKF2 500', 'NS-EnKF2 1000', 'NS-EnKF2 2000',
+#          'NS-EnKF3 50', 'NS-EnKF3 100', 'NS-EnKF3 250', 'NS-EnKF3 500', 'NS-EnKF3 1000', 'NS-EnKF3 2000',
+#          'Hyb-EnKF 50', 'Hyb-EnKF 100', 'Hyb-EnKF 250', 'Hyb-EnKF 500', 'Hyb-EnKF 1000', 'Hyb-EnKF 2000',
+#          'Dam 0.2 50', 'Dam 0.2 100', 'Dam 0.2 250', 'Dam 0.2 500', 'Dam 0.2 1000', 'Dam 0.2 2000',
+#          'IEnKF1 50', 'IEnKF1 100', 'IEnKF1 250', 'IEnKF1 500', 'IEnKF1 1000', 'IEnKF1 2000',
+#          'IEnKF2 50', 'IEnKF2 100', 'IEnKF2 250', 'IEnKF2 500', 'IEnKF2 1000', 'IEnKF2 2000',
+#          'NIEnKF 50', 'NIEnKF 100', 'NIEnKF 250', 'NIEnKF 500', 'NIEnKF 1000', 'NIEnKF 2000',
+#          'NIEnKF2 50', 'NIEnKF2 100', 'NIEnKF2 250', 'NIEnKF2 500', 'NIEnKF2 1000', 'NIEnKF2 2000',]
+names = [r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',
+         r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',
+         r'Dam0.5',r'Dam0.5',r'Dam0.5',r'Dam0.50',r'Dam0.50',r'Dam0.5',
+         r'LEnKF',r'LEnKF',r'LEnKF2',r'LEnKF',r'LEnKF',r'LEnKF',
+         r'DualEnKF',r'DualEnKF',r'DualEnKF2',r'DualEnKF0',r'DualEnKF',r'DualEnKF',
+         r'Dam0.3',r'Dam0.3',r'Dam0.3',r'Dam0.3',r'Dam0.3',r'Dam0.3',
+         r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',
+         r'NS-EnKF2',r'NS-EnKF2',r'NS-EnKF2',r'NS-EnKF2',r'NS-EnKF2',r'NS-EnKF2',
+         r'NS-EnKF3',r'NS-EnKF3',r'NS-EnKF3',r'NS-EnKF3',r'NS-EnKF3',r'NS-EnKF3',
+         r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',
+         r'Dam0.2',r'Dam0.2',r'Dam0.2',r'Dam0.2',r'Dam0.2',r'Dam0.2',
+         r'IEnKF1',r'IEnKF1',r'IEnKF1',r'IEnKF1',r'IEnKF1',r'IEnKF1',
+         r'IEnKF2',r'IEnKF2',r'IEnKF2',r'IEnKF2',r'IEnKF2',r'IEnKF2',
+         r'NIEnKF',r'NIEnKF',r'NIEnKF',r'NIEnKF',r'NIEnKF',r'NIEnKF',
+         r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',
+         r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',
+         r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',]
+
+names1000 = [r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',
+             r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',
+             r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',
+             r'DualEnKF',r'DualEnKF',r'DualEnKF2',r'DualEnKF0',r'DualEnKF',r'DualEnKF',]
+
+names_methods = ['EnKF', 'NS-EnKF', 'Dam0.5', 'LEnKF', 'DualEnKF', 
+                 'Dam0.3', 'Dam0.1', 'NS-EnKF2', 'NS-EnKF3', 'Hyb-EnKF', 
+                 'Dam0.2', 'IEnKF1', 'IEnKF2', 'NIEnKF', 'NIEnKF2',
+                 'NIEnKF3','NIEnKF4']
+
+names_methods1000 = ['EnKF','Dam0.1','NS-EnKF','DualEnKF']
