cfad57a1806e58b26bfe42c5e3fcd1d5c6beaeaf
jk125262 <jkeller@eonerc.rwth-aachen.de>
2016-04-05 16:49:03 +0200

    Plotarrays: Iterative wave, and normal wavebc.
    Iterative dats, lets and nums were included. Recently, I started doing
    wavebc runs with no-flow boundary conditions. The normal EnKF run is
    included here.

--- mypackage/plot/plotarrays.py
+++ mypackage/plot/plotarrays.py
@@ -5,10 +5,10 @@ python_dir = '/home/jk125262/PythonDir_Cluster'
 
 import numpy as np
 
-num_methods = 6
+num_methods = 7
 num_jobs = num_methods*7
 
-num_methods1000 = 6
+num_methods1000 = 7
 num_jobs1000 = num_methods1000*4
 
 # Date of output directory
@@ -24,6 +24,8 @@ hybrid_dats = ['2015_10_02','2015_12_17','2015_10_02',
                '2015_10_02','2015_10_02','2015_10_02','2015_10_02']
 localisation_dats = ['2015_06_23','2015_12_17','2015_06_23',
                      '2015_06_23','2015_06_24','2015_06_24','2015_06_24']
+newiterative4_dats = ['2015_12_11','2016_02_09','2015_12_11',
+                      '2015_12_11','2015_12_11','2015_12_11','2015_12_11']
 
 normalscore_dats = ['2015_06_11','2015_06_12','2015_06_12','2015_06_19','2015_06_19','2015_06_19']
 damping_dats = ['2015_06_13','2015_06_14','2015_06_14','2015_06_22','2015_06_22','2015_06_22']
@@ -35,7 +37,6 @@ iterative_2_dats = ['2015_10_16','2015_10_16','2015_10_16','2015_10_16','2015_10
 newiterative_dats = ['2015_11_18','2015_11_18','2015_11_18','2015_11_18','2015_11_18','2015_11_18']
 newiterative2_dats = ['2015_11_23','2015_11_23','2015_11_23','2015_11_23','2015_11_23','2015_11_23']
 newiterative3_dats = ['2015_12_08','2015_12_08','2015_12_08','2015_12_09','2015_12_09','2015_12_09']
-newiterative4_dats = ['2015_12_11','2015_12_11','2015_12_11','2015_12_11','2015_12_11','2015_12_11']
 obs_10_normal_dats = ['2015_07_20','2015_07_20','2015_07_20','2015_07_20','2015_07_20','2015_07_20']
 obs_200_normal_dats = ['2015_07_22','2015_07_22','2015_07_22','2015_07_22','2015_07_22','2015_07_22']
 lowobsvar_dats = ['2015_07_21','2015_07_27','2015_07_27','2015_07_26','2015_07_26','2015_07_26']
@@ -47,6 +48,7 @@ normalscoreverynew_lets = ['b','gjj','cx','gt','kp','ol','sh']
 dual_lets = ['b','hvv','cx','gt','kp','ol','sh']
 hybrid_lets = ['b','jih','cx','gt','kp','ol','sh']
 localisation_lets = ['b','kut','cx','gt','b','cx','gt']
+newiterative4_lets = ['b','b','aln','bxz','dkl','ewx','gjj']
 
 normalscore_lets = ['gt','b','cx','b','cx','gt']
 damping_lets = ['b','b','cx','b','cx','gt']
@@ -58,7 +60,6 @@ iterative_2_lets = ['b','cx','gt','kp','ol','sh']
 newiterative_lets = ['b','cx','gt','kp','ol','sh']
 newiterative2_lets = ['b','cx','gt','kp','ol','sh']
 newiterative3_lets = ['b','aln','bxz','dkl','ewx','gjj']
-newiterative4_lets = ['b','aln','bxz','dkl','ewx','gjj']
 obs_10_normal_lets = ['b','cx','gt','kp','ol','sh']
 obs_200_normal_lets = ['b','cx','gt','kp','ol','sh']
 lowobsvar_lets = ['b','cx','gt','kp','ol','sh']
@@ -70,6 +71,7 @@ normalscoreverynew_nums = [100,100,100,100,100,100,34]
 dual_nums = [100,100,100,100,100,100,100]
 hybrid_nums = [100,100,100,100,100,100,100]
 localisation_nums = [100,100,100,100,100,100,100]
+newiterative4_nums = [99,100,88,22,16,8,4]
 
 normalscore_nums = [100,100,100,70,48,28]  
 damping_nums = [100,100,100,100,100,100]
@@ -81,40 +83,38 @@ iterative_2_nums = [100,100,100,100,100,100]
 newiterative_nums = [100,100,100,100,100,100]
 newiterative2_nums = [100,100,100,100,100,100]
 newiterative3_nums = [99,100,100,100,100,64]
-newiterative4_nums = [99,88,22,16,8,4]
 obs_10_normal_nums = [100,100,100,100,100,100]
 obs_200_normal_nums = [100,100,100,100,100,100]
 lowobsvar_nums = [100,100,100,100,100,100]
 
 dats = np.array([normal_dats, damping01_dats, normalscoreverynew_dats, dual_dats, hybrid_dats, 
-                 localisation_dats])       #bis hier: 1000er
+                 localisation_dats,newiterative4_dats])       #bis hier: 1000er
                  # normalscore_dats, damping_dats, damping03_dats, 
                  # normalscorenew_dats, damping02_dats,
                  # iterative_dats, iterative_2_dats, newiterative_dats,newiterative2_dats,
-                 # newiterative3_dats, newiterative4_dats,
+                 # newiterative3_dats,
                  # obs_10_normal_dats, obs_200_normal_dats, lowobsvar_dats])
 lets = np.array([normal_lets, damping01_lets, normalscoreverynew_lets, dual_lets, hybrid_lets, 
-                 localisation_lets])       #bis hier: 1000er
+                 localisation_lets,newiterative4_lets])       #bis hier: 1000er
                  # normalscore_lets, damping_lets, damping03_lets,
                  # normalscorenew_lets, damping02_lets,
                  # iterative_lets, iterative_2_lets, newiterative_lets,newiterative2_lets,
-                 # newiterative3_lets,newiterative4_lets,
+                 # newiterative3_lets,
                  # obs_10_normal_lets, obs_200_normal_lets, lowobsvar_lets])
 nums = np.array([normal_nums, damping01_nums, normalscoreverynew_nums, dual_nums, hybrid_nums, 
-                 localisation_nums])       #bis hier: 1000er
+                 localisation_nums,newiterative4_nums])       #bis hier: 1000er
                  # normalscore_nums, damping_nums, damping03_nums,
                  # normalscorenew_nums, damping02_nums,
                  # iterative_nums, iterative_2_nums, newiterative_nums,newiterative2_nums,
-                 # newiterative3_nums,newiterative4_nums,
+                 # newiterative3_nums,
                  # obs_10_normal_nums, obs_200_normal_nums, lowobsvar_nums])
 
 # Number of observation intervals
 num_obss = [[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],
-            [100 for i in range(6)],[100 for i in range(6)],   #Bis hier: 1000er
+            [100 for i in range(6)],[100 for i in range(6)],[5050 for i in range(6)],   #Bis hier: 1000er
             [100 for i in range(6)],[100 for i in range(6)],
             [100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],
             [100 for i in range(6)],[150 for i in range(6)],[ 55 for i in range(6)],[ 55 for i in range(5)],
-            [5050 for i in range(6)],
             [ 10 for i in range(6)],[200 for i in range(6)],
             [100 for i in range(6)]]                             #Should be almost constant
 
@@ -125,7 +125,9 @@ normalscore1000_dats = ['2015_12_05','2015_12_17','2015_12_05','2015_12_05']
 dual1000_dats = ['2015_12_05','2015_12_17','2015_12_05','2015_12_05']
 hybrid1000_dats = ['2015_12_15','2015_12_17','2015_12_15','2015_12_15']
 localisation1000_dats = ['2015_12_18','2015_12_17','2015_12_18','2015_12_18']
-iterative1000_dats = ['2016_01_07','2016_01_14']
+iterative1000_dats = ['2016_01_07','2016_01_14','2016_01_21','2016_01_28']
+
+normal1000_wavebc_dats = ['2016_03_24','2016_03_24','2016_03_24','2016_03_24']
 
 normal1000_lets = ['b','mhf','aln','bxz']
 damping1000_lets = ['b','ntr','aln','bxz']
@@ -133,7 +135,9 @@ normalscore1000_lets = ['b','pgd','aln','bxz']
 dual1000_lets = ['dkl','qsp','ewx','gjj']
 hybrid1000_lets = ['b','sfb','aln','bxz']
 localisation1000_lets = ['b','trn','aln','bxz']
-iterative1000_lets = ['b','b']
+iterative1000_lets = ['b','b','b','b']
+
+normal1000_wavebc_lets = ['b','aln','bxz','dkl']
 
 normal1000_nums = [1000,1000,1000,1000]
 damping1000_nums = [1000,1000,1000,1000]
@@ -141,18 +145,21 @@ normalscore1000_nums = [1000,1000,1000,1000]
 dual1000_nums = [1000,1000,1000,1000]
 hybrid1000_nums = [1000,1000,1000,1000]
 localisation1000_nums = [1000,1000,1000,1000]
-iterative1000_nums = [1000,1000]
+iterative1000_nums = [1000,1000,1000,1000]
 
-## Iterative hinzufuegen
+normal1000_wavebc_nums = [1000,1000,1000,1000]
+
+
 dats1000 = [normal1000_dats,damping1000_dats,normalscore1000_dats,dual1000_dats,hybrid1000_dats,
-            localisation1000_dats]
+            localisation1000_dats, iterative1000_dats]
 lets1000 = [normal1000_lets,damping1000_lets,normalscore1000_lets,dual1000_lets,hybrid1000_lets,
-            localisation1000_lets]
+            localisation1000_lets, iterative1000_lets]
 nums1000 = [normal1000_nums,damping1000_nums,normalscore1000_nums,dual1000_nums,hybrid1000_nums,
-            localisation1000_nums]
+            localisation1000_nums, iterative1000_nums]
 
 num_obss1000 = [[100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],
-                [100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)]]
+                [100 for i in range(6)],[100 for i in range(6)],[100 for i in range(6)],
+                [5050 for i in range(6)]]
 
 # Flat arrays---------------------------------------------------------------
 dates = np.concatenate(dats)           #Dates
@@ -185,6 +192,7 @@ names = [r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',
          r'DualEnKF',r'DualEnKF',r'DualEnKF2',r'DualEnKF0',r'DualEnKF',r'DualEnKF',
          r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',
          r'LEnKF',r'LEnKF',r'LEnKF2',r'LEnKF',r'LEnKF',r'LEnKF',
+         r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',
          # Bis hier: 1000er
          r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',
          r'Dam0.5',r'Dam0.5',r'Dam0.5',r'Dam0.50',r'Dam0.50',r'Dam0.5',
@@ -195,23 +203,33 @@ names = [r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',
          r'IEnKF2',r'IEnKF2',r'IEnKF2',r'IEnKF2',r'IEnKF2',r'IEnKF2',
          r'NIEnKF',r'NIEnKF',r'NIEnKF',r'NIEnKF',r'NIEnKF',r'NIEnKF',
          r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',r'NIEnKF2',
-         r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',
-         r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',r'NIEnKF4',]
+         r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',r'NIEnKF3',]
 
 names_methods = ['EnKF', 'Dam0.1', 'NS-EnKF3', 'DualEnKF', 'Hyb-EnKF', 
-                 'LEnKF',                 # Bis hier: 1000er
+                 'LEnKF',  'NIEnKF4'               # Bis hier: 1000er
                  'NS-EnKF', 'Dam0.5', 
                  'Dam0.3', 'NS-EnKF2', 
                  'Dam0.2', 'IEnKF1', 'IEnKF2', 'NIEnKF', 'NIEnKF2',
-                 'NIEnKF3','NIEnKF4']
+                 'NIEnKF3',]
 
 
 ## Iterative hinzufuegen
 names1000 = [r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',
              r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',r'Dam0.1',
              r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',r'NS-EnKF',
-             r'DualEnKF',r'DualEnKF',r'DualEnKF2',r'DualEnKF0',r'DualEnKF',r'DualEnKF',
-             r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF2',r'Hyb-EnKF0',r'Hyb-EnKF',r'Hyb-EnKF',
-             r'LEnKF',r'LEnKF',r'LEnKF2',r'LEnKF0',r'LEnKF',r'LEnKF',]
+             r'DualEnKF',r'DualEnKF',r'DualEnKF',r'DualEnKF',r'DualEnKF',r'DualEnKF',
+             r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',r'Hyb-EnKF',
+             r'LEnKF',r'LEnKF',r'LEnKF',r'LEnKF',r'LEnKF',r'LEnKF',
+             r'IEnKF',r'IEnKF',r'IEnKF',r'IEnKF',r'IEnKF',r'IEnKF',]
 
-names_methods1000 = ['EnKF','Dam0.1','NS-EnKF','DualEnKF','Hyb-EnKF','LEnKF']
+longnames1000 = [r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',r'EnKF',
+             r'Damped EnKF',r'Damped EnKF',r'Damped EnKF',r'Damped EnKF',r'Damped EnKF',r'Damped EnKF',
+             r'Normal Score EnKF',r'Normal Score EnKF',r'Normal Score EnKF',r'Normal Score EnKF',r'Normal Score EnKF',r'Normal Score EnKF',
+             r'Dual EnKF',r'Dual EnKF',r'Dual EnKF',r'Dual EnKF',r'Dual EnKF',r'Dual EnKF',
+             r'Hybrid EnKF',r'Hybrid EnKF',r'Hybrid EnKF',r'Hybrid EnKF',r'Hybrid EnKF',r'Hybrid EnKF',
+             r'Local EnKF',r'Local EnKF',r'Local EnKF',r'Local EnKF',r'Local EnKF',r'Local EnKF',
+             r'Iterative EnKF',r'Iterative EnKF',r'Iterative EnKF',r'Iterative EnKF',r'Iterative EnKF',r'Iterative EnKF',]
+
+names_methods1000 = ['EnKF','Dam0.1','NS-EnKF','DualEnKF','Hyb-EnKF','LEnKF','IEnKF']
+
+longnames_methods1000 = ['EnKF','Damped EnKF','Normal Score EnKF','Dual EnKF','Hybrid EnKF','Local EnKF','Iterative EnKF']
