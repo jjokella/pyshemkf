02e0c0eb15ac36c4185735cc56524d81b8afa0a0
jk125262 <jkeller@eonerc.rwth-aachen.de>
2016-12-05 18:54:44 +0100

    grids: Read delx/dely from output directory

--- mypackage/plot/grids.py
+++ mypackage/plot/grids.py
@@ -19,6 +19,8 @@ import matplotlib.mlab as mlab
 import matplotlib.colors as colors
 from mpl_toolkits.axes_grid import make_axes_locatable
 
+from mypackage.run import runmodule as rm
+
 import pylab
 import vtk
 from vtk.util.numpy_support import vtk_to_numpy
@@ -26,110 +28,80 @@ from vtk.util.numpy_support import vtk_to_numpy
 import re
 import string
 
-def x(model_name):
+def x(model_name,dat,let):
     """
     Read model name and return the corresponding
     array of x-coordinates of cell centers.
     """
-    delxarr = delx(model_name)
+    delxarr = delx(model_name,dat,let)
     arr = np.array([np.sum(delxarr[:i])+0.5*delxarr[i] for i in range(delxarr.size)])
     return arr
 
 
-def y(model_name):
+def y(model_name,dat,let):
     """
     Read model name and return the corresponding
     array of y-coordinates of cell centers.
     """
-    delyarr = dely(model_name)
+    delyarr = dely(model_name,dat,let)
     arr = np.array([np.sum(delyarr[:i])+0.5*delyarr[i] for i in range(delyarr.size)])
     return arr
 
-def xticks(model_name):
+def xticks(model_name,dat,let):
     """
     Read model name and return the corresponding
     array of x-coordinates of left side of cells.
     """
-    delxarr = delx(model_name)
+    delxarr = delx(model_name,dat,let)
     arr = np.array([np.sum(delxarr[:i]) for i in range(delxarr.size+1)])
     return arr
 
-def yticks(model_name):
+def yticks(model_name,dat,let):
     """
     Read model name and return the corresponding
     array of y-coordinates of front side of cells.
     """
-    delyarr = dely(model_name)
+    delyarr = dely(model_name,dat,let)
     arr = np.array([np.sum(delyarr[:i]) for i in range(delyarr.size+1)])
     return arr
 
-def delx(model_name):
+def delx(model_name,dat,let):
     """
-    Read model name and return the corresponding
-    array of cell lengths in x-direction.
+    Read model name, output date and letter and
+    return the corresponding array of cell lengths
+    in x-direction.
     """
-    if model_name == 'headbctest' or model_name == 'alexdiff' or model_name == 'alexdiffbig':
-        vec = [[1.0 for i in range(4)],
-                   [0.5 for i in range(4)],
-                   [0.2 for i in range(5)],
-                   [0.1 for i in range(10)],
-                   [0.01 for i in range(51)],
-                   [0.0032 for i in range(100)],
-                   [0.01 for i in range(50)],
-                   [0.1 for i in range(10)],
-                   [0.2 for i in range(5)],
-                   [0.5 for i in range(4)],
-                   [1.0 for i in range(4)]]
-    elif model_name == 'prescribedhead':
-        vec = [[1.0 for i in range(31)]]
-    elif model_name == 'cube':
-        vec = [[0.01 for i in range(24)],
-                   [0.0032 for i in range(101)],
-                   [0.01 for i in range(24)]]
-    elif model_name == 'cubey':
-        vec = [[0.08 for i in range(3)],
-                   [0.0096 for i in range(8)],
-                   [0.0032 for i in range(53)],
-                   [0.0096 for i in range(8)],
-                   [0.08 for i in range(3)]]
-    else:
-        raise exceptions.RuntimeError('Model name not valid: '+model_name) 
+    output_path = os.environ['HOME']+'/shematOutputDir/'+model_name+'_output/' \
+                  + dat + '/' + dat + '_'+ let+ '/'
+    input_file = rm.make_file_dir_names(model_name)[2]
 
+    line = rm.read_hashtag_input(output_path+input_file,'# delx',1)
+    num_entries = len(str.split(line))
+    nums = [int(  str.split(str.split(line)[i],"*")[0]) for i in range(num_entries)]
+    lens = [float(str.split(str.split(line)[i],"*")[1]) for i in range(num_entries)]
+
+    vec = [[lens[i] for j in range(nums[i]) ] for i in range(num_entries)]
+    
     arr = np.array([num for elem in vec for num in elem])
     return arr
 
-def dely(model_name):
+def dely(model_name,dat,let):
     """
-    Read model name and return the corresponding
-    array of cell lengths in y-direction.
+    Read model name, output date and letter and
+    return the corresponding array of cell lengths
+    in y-direction.
     """
-    if model_name == 'headbctest' or model_name == 'alexdiff' or model_name == 'alexdiffbig':
-        vec = [[1.0 for i in range(4)],
-                   [0.5 for i in range(4)],
-                   [0.2 for i in range(5)],
-                   [0.1 for i in range(10)],
-                   [0.01 for i in range(51)],
-                   [0.0032 for i in range(100)],
-                   [0.01 for i in range(50)],
-                   [0.1 for i in range(10)],
-                   [0.2 for i in range(5)],
-                   [0.5 for i in range(4)],
-                   [1.0 for i in range(4)]]
-    elif model_name == 'prescribedhead':
-        vec = [[1.0 for i in range(31)]]
-    elif model_name == 'cube':
-        vec = [[0.01 for i in range(24)],
-                   [0.0032 for i in range(101)],
-                   [0.01 for i in range(24)]]
-    elif model_name == 'cubey':
-        vec = [[0.08 for i in range(3)],
-                   [0.0096 for i in range(8)],
-                   [0.0032 for i in range(53)],
-                   [0.0096 for i in range(8)],
-                   [0.08 for i in range(3)]]
-    else:
-        raise exceptions.RuntimeError('Model name not valid: '+model_name) 
+    output_path = os.environ['HOME']+'/shematOutputDir/'+model_name+'_output/' \
+                  + dat + '/' + dat + '_'+ let+ '/'
+    input_file = rm.make_file_dir_names(model_name)[2]
 
+    line = rm.read_hashtag_input(output_path+input_file,'# dely',1)
+    num_entries = len(str.split(line))
+    nums = [int(  str.split(str.split(line)[i],"*")[0]) for i in range(num_entries)]
+    lens = [float(str.split(str.split(line)[i],"*")[1]) for i in range(num_entries)]
+
+    vec = [[lens[i] for j in range(nums[i]) ] for i in range(num_entries)]
+    
     arr = np.array([num for elem in vec for num in elem])
     return arr
 
