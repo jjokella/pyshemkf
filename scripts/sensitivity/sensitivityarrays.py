fdaa9a1cb7e21b567bc98c7c2de3a415613535e7
jk125262 <jkeller@eonerc.rwth-aachen.de>
2017-02-08 12:27:35 +0100

    sensitivityarrays: Exclude truelet, varranges, new run

--- scripts/sensitivity/sensitivityarrays.py
+++ scripts/sensitivity/sensitivityarrays.py
@@ -4,19 +4,34 @@
 #                      Variables for Sensitivity Analysis                     #
 ###############################################################################
 
+import numpy as np
+
 tag = "sensitivity"
 
 # Different runs ##############################################################
-runs = [['cubey','2017_01_15','a','2017_01_15',range(1000)],
-        ['cubey','2017_01_31','a','2017_01_31',range(1000)],
-        ['cubey','2017_01_31','aln','2017_01_31',range(1001,2000)],
-        ['cubey','2017_01_31','bxz','2017_01_31',range(2001,3000)],
-        ['cubey','2017_01_30','a','2017_01_30',range(1000)],
-        ['cubey','2017_01_30','aln','2017_01_30',range(1001,2000)],
-        ['cubey','2017_01_30','bxz','2017_01_30',range(2001,3000)],
-        ['cubey','2017_01_30','dkl','2017_01_30',range(3001,4000)]]
+runs = [['cubey','2017_01_15',range(1000)],
+        ['cubey','2017_01_31',range(1000)],
+        ['cubey','2017_01_31',range(1001,2000)],
+        ['cubey','2017_01_31',range(2001,3000)],
+        ['cubey','2017_01_30',range(1000)],
+        ['cubey','2017_01_30',range(1001,2000)],
+        ['cubey','2017_01_30',range(2001,3000)],
+        ['cubey','2017_01_30',range(3001,4000)]
+]
         
-
+varranges = {'cubey_2017_01_15_a':np.arange(0.5*10**6,3.5*10**6,0.003*10**6),
+             'cubey_2017_01_31_a':np.arange(0.5*10**6,3.5*10**6,0.003*10**6),
+             'cubey_2017_01_31_aln':np.arange(0.5*10**6,3.5*10**6,0.003*10**6),
+             'cubey_2017_01_31_bxz':np.arange(0.5*10**6,3.5*10**6,0.003*10**6),
+             'cubey_2017_01_30_a':np.arange(0.1,3.1,0.003),
+             'cubey_2017_01_30_aln':np.arange(0.1,3.1,0.003),
+             'cubey_2017_01_30_bxz':np.arange(0.1,3.1,0.003),
+             'cubey_2017_01_30_dkl':np.arange(0.1,3.1,0.003),
+             'cubey_2017_02_02_cx':np.arange(0.0,1.0*10**-7,0.1*10**-7),
+             'cubey_2017_02_02_dh':np.arange(0.0,1.0*10**-6,0.1*10**-6),
+             'cubey_2017_02_02_dr':np.arange(0.0,1.0*10**-5,0.1*10**-5),
+             
+}
 
 
 # Sensitivity analysis: Varied variables ######################################
@@ -46,7 +61,8 @@ sensitivity_varnames = {'cubey_2016_12_13_a':'Thermal conductivity deprecated',
                         'cubey_2017_02_02_cd':'Thermal conductivity v= 10**-5',
                         'cubey_2017_02_02_cx':'Velocity',
                         'cubey_2017_02_02_dh':'Velocity',
-                        'cubey_2017_02_02_dr':'Velocity'}
+                        'cubey_2017_02_02_dr':'Velocity',
+}
 
 # Cubey: Unit numbers #########################################################
 unit_numbers = {'cubey_2016_12_13_a': 1,
@@ -75,14 +91,16 @@ unit_numbers = {'cubey_2016_12_13_a': 1,
                 'cubey_2017_02_02_cd':7,
                 'cubey_2017_02_02_cx':0,
                 'cubey_2017_02_02_dh':0,
-                'cubey_2017_02_02_dr':0}
+                'cubey_2017_02_02_dr':0,
+}
 
 # Cubey: Unit names ###########################################################
 unit_names =  {0:'Velocity, bc',
                1:'Sand, outside',
                2:'Sand, inside',
                7:'Hull, large tube',
-               3:'Inside, small tube'}
+               3:'Inside, small tube',
+}
 
 # Sensitivity analysis: Strings of Ranges #####################################
 sensitivity_ranges = {'cubey_2016_12_13_a':r'1.5-3.0 $\frac{W}{mK}$ deprecated',
@@ -111,7 +129,8 @@ sensitivity_ranges = {'cubey_2016_12_13_a':r'1.5-3.0 $\frac{W}{mK}$ deprecated',
                       'cubey_2017_02_02_cd':r'0.1-3.1 $\frac{W}{mK}$',
                       'cubey_2017_02_02_cx':r'0.0-1.0 $10^{-7} \frac{m}{s}$',
                       'cubey_2017_02_02_dh':r'0.0-1.0 $10^{-6} \frac{m}{s}$',
-                      'cubey_2017_02_02_dr':r'0.0-1.0 $10^{-5} \frac{m}{s}$',}
+                      'cubey_2017_02_02_dr':r'0.0-1.0 $10^{-5} \frac{m}{s}$',
+}
 
 
 # Sensitivity analysis: Default numbers #######################################
@@ -134,7 +153,8 @@ default_jobs = {'cubey_2017_01_15_a':101,
                 'cubey_2017_02_02_cd':6,
                 'cubey_2017_02_02_cx':0,
                 'cubey_2017_02_02_dh':0,
-                'cubey_2017_02_02_dr':0,}
+                'cubey_2017_02_02_dr':0,
+}
 
 default_values = {'cubey_2017_01_15_a':r'0.8 $10^6 \frac{J}{Km^3}$',
                   'cubey_2017_01_31_a':r'0.8 $10^6 \frac{J}{Km^3}$',
@@ -155,7 +175,8 @@ default_values = {'cubey_2017_01_15_a':r'0.8 $10^6 \frac{J}{Km^3}$',
                   'cubey_2017_02_02_cd':r'2.0 $\frac{W}{mK}$',
                   'cubey_2017_02_02_cx':r'0.0 $\frac{m}{s}$',
                   'cubey_2017_02_02_dh':r'0.0 $\frac{m}{s}$',
-                  'cubey_2017_02_02_dr':r'0.0 $\frac{m}{s}$',}
+                  'cubey_2017_02_02_dr':r'0.0 $\frac{m}{s}$',
+}
 
 
 # CUBEY: Measurement points ###################################################
@@ -174,7 +195,8 @@ obs_longlabels = {0:'North(out)',            # Outer ring
                   12:'Sinh(in)',
                   13:'Sinheast(in)',
                   14:'East(in)',
-                  15:'Northeast(in)',}
+                  15:'Northeast(in)',
+}
 
 obs_shortlabels = {0:'NN',            # Outer ring
                    1:'NW',
@@ -191,7 +213,8 @@ obs_shortlabels = {0:'NN',            # Outer ring
                    12:'ss',
                    13:'se',
                    14:'ee',
-                   15:'ne',}
+                   15:'ne',
+}
 
 obs_difflabels = {0:'NN-SS',    # Outer ring
                   1:'NW-SE',
@@ -200,4 +223,5 @@ obs_difflabels = {0:'NN-SS',    # Outer ring
                   4:'nn-ss',    # Inner ring
                   5:'nw-se',
                   6:'ww-ee',
-                  7:'sw-ne'}
+                  7:'sw-ne'
+}
