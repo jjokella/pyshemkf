92b9d2f1fdfd60de36469a0c2f4c425142d3fccd
jk125262 <jkeller@eonerc.rwth-aachen.de>
2017-04-28 12:57:05 +0200

    mypackage: sensitivity/mix ported to mypackage

--- mypackage/sensitivity/read.py
+++ mypackage/sensitivity/read.py
@@ -70,3 +70,73 @@ def read(model_name,dat,let,varname="temp"):
 
     return var, array_name
 
+
+
+def mix(model_name,dat,lets,is_diff = False):
+    """
+    Generating sensitivity array holding temperature (difference)
+    profiles for varying parameter values.
+
+    Parameters
+    ----------
+    model_name : string
+        String of model name.
+
+    dat : string
+        String with date of model run.
+
+    let : string
+        String of letter of model run.
+
+    is_diff : logical
+        If yes, create an array of temperature differences.
+
+    Returns
+    -------
+    sense : array
+        Temperature profiles for different parameter values.
+
+        sense(it,imons,ilet): Transient Temperature profiles (at locations,
+        for different parameter values)
+
+        sensedt(it,imons,ilet): Transient Temperature difference profiles
+        (at locations, for different parameter values)
+
+    sense_name : string
+        String containing a proposed saving location for sense.
+    """
+
+    # Read model parameters
+    num_mons = sc.num_mons(model_name,dat,lets[0])
+    nt = sc.nt(model_name,dat,lets[0])
+
+    # Empty sensitivity arrays
+    if not is_diff:
+        sense = np.zeros([nt,num_mons,len(lets)])
+    else:
+        sense = np.zeros([nt,num_mons/2,len(lets)])
+
+
+    # Loop for letters/parameter values
+    for il,let in enumerate(lets):
+
+        # Load temperature array
+        temp = np.load(pm.py_output_filename(sa.tag,"truetemp",sc.specl(model_name,dat,let),"npy"))
+
+        if not is_diff:
+            # Fill sense with temperature data
+            sense[:,:,il] = temp[:,:]
+        else:
+            # Fill sense with temperature difference data
+            for j in range(num_mons/2):
+                sense[:,j,il] = temp[:,j] - temp[:,j+4]
+
+
+    # Name
+    if not is_diff:
+        sense_name = pm.py_output_filename(sa.tag,"sense",sc.spec(),"npy")
+    else:
+        sense_name = pm.py_output_filename(sa.tag,"sensedt",sc.spec(),"npy")
+
+    return sense, sense_name
+
