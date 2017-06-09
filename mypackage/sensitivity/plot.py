3bb643ca21147d3203260de588821e10a8a5b3db
jk125262 <jkeller@eonerc.rwth-aachen.de>
2017-06-09 16:53:41 +0200

    mypackage: dnplot

--- mypackage/sensitivity/plot.py
+++ mypackage/sensitivity/plot.py
@@ -335,6 +335,127 @@ def nplot(ax,model_name,dat,let,
     return ax, pic_name
 
 ###############################################################################
+#        Plot sensitiviy curves for different parameters differences          #
+###############################################################################
+
+def dnplot(ax,model_name,dat,let,
+               imons = [0,9],
+               pic_format = 'pdf',              # png, eps, pdf
+               title = "Default",
+               position = [0.1,0.1,0.8,0.8],
+               xlims = [10,30*24*3600],
+               ylims = [-0.01,0.15],
+               color = 'black',
+               is_labels = True,
+               is_legend = True,
+               is_fourier = False,
+               ):
+    """
+    A plotting function for temperature difference sensitivity scores.
+
+    Parameters
+    ----------
+    ax : Axes
+        The axes to draw to.
+
+    model_name : string
+        String of model name.
+
+    dat : string
+        String with date of model run.
+
+    let : string
+        String of letter of model run.
+
+    Returns
+    -------
+    ax : Axes
+        Axes instance with plots.
+
+    pic_name : string
+        Containing proposed saving location for Figure.
+    """
+
+    fname_body = 'sense'
+
+    pic_name_start = 'num'+fname_body+'dt_'+str(imons[0]).zfill(2)+'_'+str(imons[1]).zfill(2)
+    numsense = np.load(
+        pm.py_output_filename(
+            sv.tag,
+            "num"+fname_body+"_"+str(imons[0]).zfill(2),
+            sc.specl(model_name,dat,let),
+            "npy"
+            )
+        )-np.load(
+            pm.py_output_filename(
+                sv.tag,
+                "num"+fname_body+"_"+str(imons[1]).zfill(2),
+                sc.specl(model_name,dat,let),
+                "npy"
+                )
+            )
+    numsense_label = np.core.defchararray.add(
+        np.load(pm.py_output_filename(
+            sv.tag,
+            "num"+fname_body+"_label_"+str(imons[0]).zfill(2),
+            sc.specl(model_name,dat,let),
+            "npy")),
+        np.load(pm.py_output_filename(
+            sv.tag,
+            "num"+fname_body+"_label_"+str(imons[1]).zfill(2),
+            sc.specl(model_name,dat,let),
+            "npy")),
+        )
+
+    t = np.load(pm.py_output_filename(sv.tag,
+                                      "truet",
+                                      sc.specl(model_name,dat,let),
+                                      "npy"))
+
+    # Title
+    if not title=="Default":
+        ax.set_title(title,
+                     size = 20)
+    else:
+        ax.set_title(
+            'Sensitivity numbers at '+sa.obs_longlabels[imons[0]]+' minus '+sa.obs_longlabels[imons[1]],
+            size = 20
+            )
+
+    # Axis position
+    ax.set_position(position)
+
+    # Plot
+    ax.semilogx(t,numsense, 'o',
+                    color = color, # White to black
+                    linestyle = '--',
+                    label=numsense_label,
+                    markersize = 1.0,
+                    markeredgewidth = 0.1)
+
+    if is_labels:
+        ax.axhline(0,c='k',linestyle=':',linewidth=0.3)
+        ax.set_xlabel(r'Time',fontsize = 14, labelpad=0)
+        ax.set_ylabel(r'Temperature difference [$ ^{\circ} C$]',fontsize = 14)
+        ax.xaxis.set_ticks([10,60,600,3600,24*3600,10*24*3600])
+        ax.xaxis.set_ticklabels(['10s','1min','10min','1h','1d','10d'])
+        ax.set_xlim(xlims[0],xlims[1])
+        ax.set_ylim(ylims[0],ylims[1])
+
+    if is_legend:
+        ax.legend(markerscale=2.0,
+                  loc = 'upper left',
+                  fontsize=6,
+                  ncol=3)
+
+    if is_fourier:
+        ax.vlines(41780,ylims[0],ylims[1],linestyles='dashed')
+
+    pic_name = pm.py_output_filename(sv.tag,pic_name_start,sc.specl(model_name,dat,let),pic_format)
+
+    return ax, pic_name
+
+###############################################################################
 #                            Plot diffusivity curve                           #
 ###############################################################################
 
@@ -350,7 +471,7 @@ def aplot(
         pic_format = 'pdf',              # png, eps, pdf
         xlims = [0,1],
         ylims = [0,4],
-     ):
+        ):
     """
     A plotting function for diffusivity curve vs porosity
 
